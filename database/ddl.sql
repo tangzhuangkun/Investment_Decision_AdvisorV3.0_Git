27e9eba78edfc12f43628b4ea46f65fede1cf763
AdhocSun <1029791755@qq.com>
2020-09-01 22:11:20 +0800

    完善当前文档

--- database/ddl.sql
+++ database/ddl.sql
@@ -14,7 +14,7 @@ FLUSH PRIVILEGES;
 /* --------- db：parser_component ------ */
 /*创建一个表，IP_availability，用于记录ip的可用性 */
 
-
+USE parser_component;
 CREATE TABLE IF NOT EXISTS `IP_availability`(
 	`ip_address` VARCHAR(21) NOT NULL COMMENT '主键，ip的地址+端口号，最长可达21位，作为主键可确保数据库中的ip地址不会重复',
 	`is_anonymous` BOOLEAN NOT NULL COMMENT '是否为高匿名，是为1，否为0',
@@ -34,6 +34,7 @@ COMMENT '记录IP的可用性';
 /* --------- db：parser_component ------ */
 /*创建一个表，fake_user_agent，用于存储生成的假UA（用户代理）*/
 
+USE parser_component;
 CREATE TABLE IF NOT EXISTS `fake_user_agent`(
 	`id` MEDIUMINT NOT NULL AUTO_INCREMENT,
 	`ua` VARCHAR(1000) NOT NULL COMMENT '用户代理',
@@ -63,7 +64,7 @@ FLUSH PRIVILEGES;
 /* --------- db：financial_data ------ */
 /*创建一个表，index_constituent_stocks_weights，用于存储 指数构成及权重*/
 
-
+USE financial_data;
 CREATE TABLE IF NOT EXISTS `index_constituent_stocks_weight`(
 	`id` MEDIUMINT NOT NULL AUTO_INCREMENT,
 	`index_code` VARCHAR(12) NOT NULL COMMENT '指数代码',
