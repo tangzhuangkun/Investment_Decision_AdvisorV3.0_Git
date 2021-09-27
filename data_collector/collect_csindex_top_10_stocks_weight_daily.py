6dafbc4a2ec4c06c64c03dc639c661f9fe808ef3
AdhocSun <1029791755@qq.com>
2021-09-27 22:16:17 +0800

    1、收集中证指数时，收集前十权重股公布的截止日期

--- data_collector/collect_csindex_top_10_stocks_weight_daily.py
+++ data_collector/collect_csindex_top_10_stocks_weight_daily.py
@@ -124,14 +124,14 @@ class CollectCSIndexTop10StocksWeightDaily:
     def collect_target_index_stock_info(self):
         # 收集目标池中的中证指数每日前十大权重股构成,并存入数据库
 
-        # 获取今天日期
-        today = time.strftime("%Y-%m-%d", time.localtime())
         # 获取标的池中关于 中证公司的 指数代码及指数名称
         cs_target_indexes_names_dict = read_collect_target_fund.ReadCollectTargetFund().get_given_index_company_index("中证")
         # 遍历这些指数
         for target_index_code in cs_target_indexes_names_dict:
             # 获取 该指数的最新更新日期及前十成份股信息
             stocks_info = self.get_single_index_latest_constituent_stock_and_weight(target_index_code[:6])
+            # 截止日期
+            expiration_date = stocks_info[0]
             # 指数代码，如 399997
             index_code = target_index_code[:6]
             # 指数名称，如 中证白酒
@@ -146,7 +146,7 @@ class CollectCSIndexTop10StocksWeightDaily:
                 try:
                     # 插入的SQL
                     inserting_sql = "INSERT INTO index_constituent_stocks_weight(index_code,index_name,stock_code,stock_name,weight,source,submission_date)" \
-                                    "VALUES ('%s','%s','%s','%s','%s','%s','%s')" % (index_code, index_name, stock_code, stock_name,weight, '中证',today)
+                                    "VALUES ('%s','%s','%s','%s','%s','%s','%s')" % (index_code, index_name, stock_code, stock_name,weight, '中证',expiration_date)
                     db_operator.DBOperator().operate("insert", "financial_data", inserting_sql)
 
                 except Exception as e:
