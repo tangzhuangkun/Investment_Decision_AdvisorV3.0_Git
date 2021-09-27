5a6b16c70c585cafa7dc75c7ba110e9583da542a
AdhocSun <1029791755@qq.com>
2021-09-28 00:07:55 +0800

    1、根据最新的指数成分和股票历史估值信息，运行mysql脚本，计算指数在历史上每一天的估值情况

--- data_miner/cal_index_his_estimation.sql
+++ data_miner/cal_index_his_estimation.sql
@@ -1,5 +1,3 @@
-# todo 插入后的结果，取出某一天，需要与excel计算结果对比
-
 # 利用mysql，基于指数现在的成分股进行计算历史估值信息
 # 将计算结果插入另外一个的表中
 insert into aggregated_data.index_components_historical_estimations (index_code, index_name, historical_date, pe_ttm, pe_ttm_effective_weight,
@@ -8,110 +6,110 @@ pcf_ttm, pcf_ttm_effective_weight, dividend_yield, dividend_yield_effective_weig
 
 select a.index_code, a.index_name, c.date as historical_date,
 # 计算指数pe_ttm
-sum(
+round(sum(
 case
 		when pe_ttm < 0 then  0
 	else
 		weight*pe_ttm/100
 	end
-) as pe_ttm,
+),5) as pe_ttm,
 # 计算指数pe_ttm的有效权重，舍弃pe_ttm<=0的成份股权重
-sum(
+round(sum(
 	case
 		when pe_ttm < 0 then  0
 	else
 		weight
-	end ) as pe_ttm_effective_weight,
+	end ),5) as pe_ttm_effective_weight,
 # 计算指数pe_ttm_nonrecurring
-sum(
+round(sum(
 case
 		when pe_ttm_nonrecurring < 0 then  0
 	else
 		weight*pe_ttm_nonrecurring/100
 	end
-) as pe_ttm_nonrecurring,
+),5) as pe_ttm_nonrecurring,
 # 计算指数pe_ttm_nonrecurring的有效权重，舍弃pe_ttm_nonrecurring<=0的成份股权重
-sum(
+round(sum(
 	case
 		when pe_ttm_nonrecurring < 0 then  0
 	else
 		weight
-	end ) as pe_ttm_nonrecurring_effective_weight,
+	end ),5) as pe_ttm_nonrecurring_effective_weight,
 # 计算指数pb
-sum(
+round(sum(
 case
 		when pb < 0 then  0
 	else
 		weight*pb/100
 	end
-) as pb,
+),5) as pb,
 # 计算指数pb的有效权重，舍弃pb<=0的成份股权重
-sum(
+round(sum(
 	case
 		when pb < 0 then  0
 	else
 		weight
-	end ) as pb_effective_weight,
+	end ),5) as pb_effective_weight,
 # 计算指数pb_wo_gw
-sum(
+round(sum(
 case
 		when pb_wo_gw < 0 then  0
 	else
 		weight*pb_wo_gw/100
 	end
-) as pb_wo_gw,
+),5) as pb_wo_gw,
 # 计算指数pb_wo_gw的有效权重，舍弃pb_wo_gw<=0的成份股权重
-sum(
+round(sum(
 	case
 		when pb_wo_gw < 0 then  0
 	else
 		weight
-	end ) as pb_wo_gw_effective_weight,
+	end ),5) as pb_wo_gw_effective_weight,
 # 计算指数ps_ttm
-sum(
+round(sum(
 case
 		when ps_ttm < 0 then  0
 	else
 		weight*ps_ttm/100
 	end
-) as ps_ttm,
+),5) as ps_ttm,
 # 计算指数ps_ttm的有效权重，舍弃ps_ttm<=0的成份股权重
-sum(
+round(sum(
 	case
 		when ps_ttm < 0 then  0
 	else
 		weight
-	end ) as ps_ttm_effective_weight,
+	end ),5) as ps_ttm_effective_weight,
 # 计算指数pcf_ttm
-sum(
+round(sum(
 case
 		when pcf_ttm < 0 then  0
 	else
 		weight*pcf_ttm/100
 	end
-) as pcf_ttm,
+),5) as pcf_ttm,
 # 计算指数pcf_ttm的有效权重，舍弃pcf_ttm<=0的成份股权重
-sum(
+round(sum(
 	case
 		when pcf_ttm < 0 then  0
 	else
 		weight
-	end ) as pcf_ttm_effective_weight,
+	end ),5) as pcf_ttm_effective_weight,
 # 计算指数dividend_yield
-sum(
+round(sum(
 case
 		when dividend_yield< 0 then  0
 	else
 		weight*dividend_yield/100
 	end
-) as dividend_yield,
+),5) as dividend_yield,
 # 计算指数dividend_yield的有效权重，舍弃dividend_yield<=0的成份股权重
-sum(
+round(sum(
 	case
 		when dividend_yield< 0 then  0
 	else
 		weight
-	end ) as dividend_yield_effective_weight
+	end ),5) as dividend_yield_effective_weight
 from
 # 获取指数及其成分股，权重，日期
 (select index_code, index_name, stock_code, stock_name, submission_date, weight
@@ -130,4 +128,4 @@ inner join
 from stocks_main_estimation_indexes_historical_data) c
 on a.stock_code = c.stock_code
 group by c.date,a.index_code, a.index_name
-order by c.date desc;
+order by c.date desc
