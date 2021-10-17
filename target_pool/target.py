927ec144f605dfd3368a58bf0719f7026c68fafa
AdhocSun <1029791755@qq.com>
2021-10-18 00:00:41 +0800

    1、目标池中的指数代码保留交易所代码

--- target_pool/target.py
+++ target_pool/target.py
@@ -1,4 +1,3 @@
-
 # 基金标的池
 
 # fund_code : 基金代码
@@ -14,16 +13,33 @@
 # monitoring_frequency_list :  监控频率, daily, weekly, monthly, seasonly, yearly, periodically
 
 target_index_funds = {
-				'160222':{'fund_name':'国泰国证食品饮料行业指数', 'fund_type':'股票指数 ', 'tracking_index_name':'国证食品', 'tracking_index_code':'399396', 'index_company':'国证', 'hold_or_not':True, 'valuation_method_list':['pe'], 'B&H_strategy_list':[], 'sell_out_strategy_list':[], 'monitoring_frequency_list':['daily']},
+	'161725': {'fund_name': '招商中证白酒指数分级', 'fund_type': '股票指数', 'tracking_index_name': '中证白酒',
+			   'tracking_index_code': '399997.XSHE', 'index_company': '中证', 'hold_or_not': True,
+			   'valuation_method_list': ['pe'], 'B&H_strategy_list': [], 'sell_out_strategy_list': [],
+			   'monitoring_frequency_list': ['daily']},
 
-				'159928':{'fund_name':'汇添富中证主要消费ETF', 'fund_type':'股票指数,ETF-场内', 'tracking_index_name':'中证主要消费', 'tracking_index_code':'000932', 'index_company':'中证', 'hold_or_not':True, 'valuation_method_list':['pe'], 'B&H_strategy_list':[], 'sell_out_strategy_list':[], 'monitoring_frequency_list':['daily']},
-				
-				'161725':{'fund_name':'招商中证白酒指数分级', 'fund_type':'股票指数', 'tracking_index_name':'中证白酒', 'tracking_index_code':'399997', 'index_company':'中证', 'hold_or_not':True, 'valuation_method_list':['pe'], 'B&H_strategy_list':[], 'sell_out_strategy_list':[], 'monitoring_frequency_list':['daily']},
+    '160222': {'fund_name': '国泰国证食品饮料行业指数', 'fund_type': '股票指数 ', 'tracking_index_name': '国证食品',
+               'tracking_index_code': '399396.XSHE', 'index_company': '国证', 'hold_or_not': True,
+               'valuation_method_list': ['pe'], 'B&H_strategy_list': [], 'sell_out_strategy_list': [],
+               'monitoring_frequency_list': ['daily']},
 
-				'160628':{'fund_name':'鹏华地产分级', 'fund_type':'股票指数', 'tracking_index_name':'中证800地产', 'tracking_index_code':'399965', 'index_company':'中证', 'hold_or_not':True, 'valuation_method_list':['pb'], 'B&H_strategy_list':[], 'sell_out_strategy_list':[], 'monitoring_frequency_list':['daily']},
+    '159928': {'fund_name': '汇添富中证主要消费ETF', 'fund_type': '股票指数,ETF-场内', 'tracking_index_name': '中证主要消费',
+               'tracking_index_code': '000932.XSHG', 'index_company': '中证', 'hold_or_not': True,
+               'valuation_method_list': ['pe'], 'B&H_strategy_list': [], 'sell_out_strategy_list': [],
+               'monitoring_frequency_list': ['daily']},
 
-				'160517':{'fund_name':'博时中证银行指数(LOF)', 'fund_type':'股票指数', 'tracking_index_name':'中证银行指数', 'tracking_index_code':'399986', 'index_company':'中证', 'hold_or_not':True, 'valuation_method_list':['pb'], 'B&H_strategy_list':[], 'sell_out_strategy_list':[], 'monitoring_frequency_list':['daily']},
+    '160628': {'fund_name': '鹏华地产分级', 'fund_type': '股票指数', 'tracking_index_name': '中证800地产',
+               'tracking_index_code': '399965.XSHE', 'index_company': '中证', 'hold_or_not': True,
+               'valuation_method_list': ['pb'], 'B&H_strategy_list': [], 'sell_out_strategy_list': [],
+               'monitoring_frequency_list': ['daily']},
 
-				'510630':{'fund_name':'华夏消费ETF', 'fund_type':'股票指数', 'tracking_index_name':'上证主要消费行业指数', 'tracking_index_code':'000036', 'index_company':'中证', 'hold_or_not':False, 'valuation_method_list':['pe'], 'B&H_strategy_list':[], 'sell_out_strategy_list':[], 'monitoring_frequency_list':['daily']}
-				}
-				
+    '160517': {'fund_name': '博时中证银行指数(LOF)', 'fund_type': '股票指数', 'tracking_index_name': '中证银行指数',
+               'tracking_index_code': '399986.XSHE', 'index_company': '中证', 'hold_or_not': True,
+               'valuation_method_list': ['pb'], 'B&H_strategy_list': [], 'sell_out_strategy_list': [],
+               'monitoring_frequency_list': ['daily']},
+
+    '510630': {'fund_name': '华夏消费ETF', 'fund_type': '股票指数', 'tracking_index_name': '上证主要消费行业指数',
+               'tracking_index_code': '000036.XSHG', 'index_company': '中证', 'hold_or_not': False,
+               'valuation_method_list': ['pe'], 'B&H_strategy_list': [], 'sell_out_strategy_list': [],
+               'monitoring_frequency_list': ['daily']}
+}
