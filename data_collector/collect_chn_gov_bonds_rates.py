5e6b5c1a84202cedac12658f1167a92f2ce94917
AdhocSun <1029791755@qq.com>
2021-11-06 00:43:09 +0800

    从中国债券信息网收集 中国债券到期收益率， 完成

--- data_collector/collect_chn_gov_bonds_rates.py
+++ data_collector/collect_chn_gov_bonds_rates.py
@@ -12,28 +12,27 @@ class CollectCHNGovBondsRates:
     # 从中国债券信息网收集 中国债券到期收益率
 
     def __init__(self):
-        pass
+        # 获取当前时间
+        self.today = time.strftime("%Y-%m-%d", time.localtime())
 
     def millisecond_to_time(self, millis):
         """13位时间戳转换为日期格式字符串"""
         # millis, 13位时间戳
         return time.strftime('%Y-%m-%d', time.localtime(millis / 1000))
 
-    def call_bonds_interface_to_collect_all_historical_data(self):
-        # 调用中国债券信息网接口
+    def call_bonds_interface_to_collect_all_historical_data(self, startDay, endDay, isOnlyToday=0):
+        # 调用中国债券信息网接口,收集国债各到期品种的过往到期收益率
 
         # header，伪装的UA
         # proxy，伪装的IP
         # startDay, 开始日期， 如 2021-11-01
         # endDay, 结束日期， 如 2021-11-04（startDay和endDay不可相同）
+        # isOnlyToday,是否只收集今天，默认 否（0），选是（1）
 
-        bonds_interface_address = "https://yield.chinabond.com.cn/cbweb-mn/yc/queryYz?bjlx=no&&dcq=0.083333,1m;0.166667,2m;0.25,3m;0.5,6m;0.75,9m;1,1y;2,2y;3,3y;5,5y;7,7y;10,10y;&&startTime=2021-10-01&&endTime=2021-11-04&&qxlx=0,&&yqqxN=N&&yqqxK=K&&par=day&&ycDefIds=2c9081e50a2f9606010a3068cae70001,&&locale=zh_CN"
+        bonds_interface_address = "https://yield.chinabond.com.cn/cbweb-mn/yc/queryYz?bjlx=no&&dcq=0.083333,1m;0.166667,2m;0.25,3m;0.5,6m;0.75,9m;1,1y;2,2y;3,3y;5,5y;7,7y;10,10y;&&startTime="+startDay+"&&endTime="+endDay+"&&qxlx=0,&&yqqxN=N&&yqqxK=K&&par=day&&ycDefIds=2c9081e50a2f9606010a3068cae70001,&&locale=zh_CN"
 
         # 解决报错 InsecureRequestWarning: Unverified HTTPS request is being made
         requests.packages.urllib3.disable_warnings()
-
-        # 获取当前时间
-        today = time.strftime("%Y-%m-%d", time.localtime())
 
         # 伪装，隐藏UA和IP
         ip_address, ua = disguise.Disguise().get_one_IP_UA()
@@ -47,6 +46,9 @@ class CollectCHNGovBondsRates:
         # [{"ycDefId":"2c9081e50a2f9606010a3068cae700015.0","ycDefName":"中债国债收益率曲线(到期)(5y)","ycYWName":null,"worktime":"","seriesData":[[1635868800000,2.7986],[1635955200000,2.7759]],"isPoint":false,"hyCurve":false,"point":false},{"ycDefId":"2c9081e50a2f9606010a3068cae7000110.0","ycDefName":"中债国债收益率曲线(到期)(10y)","ycYWName":null,"worktime":"","seriesData":[[1635868800000,2.9385],[1635955200000,2.9261]],"isPoint":false,"hyCurve":false,"point":false},{"ycDefId":"yzdcqx","ycDefName":"点差曲线","ycYWName":null,"worktime":null,"seriesData":[[1635868800000,0.1399],[1635955200000,0.1502]],"isPoint":false,"hyCurve":false,"point":false}]
         data_json_list = json.loads(raw_page)
 
+        # 国债到期期限排序
+        term_order_dict = {0:'1m', 1:'2m', 2:'3m', 3:'6m', 4:'9m', 5:'1y', 6:'2y', 7:'3y', 8:'5y', 9:'7y', 10:'10y'}
+
         # 遍历 国债各时间到期信息
         for i in range(len(data_json_list)):
             # 每组信息如下
@@ -54,49 +56,86 @@ class CollectCHNGovBondsRates:
 
             # 第一组到期信息，需要插入数据库
             if i == 0:
-                for j in data_json_list[i]["seriesData"]:
+                # 优先插入最新日期的数据
+                for j in range(len(data_json_list[i]["seriesData"]) - 1, -1+isOnlyToday, -1):
                     # 时间和利率信息如下
                     # [[1633622400000,1.776],[1633708800000,1.7659],,,]
-                    p_day = self.millisecond_to_time(j[0])
-                    rate = j[1]
-
+                    p_day = self.millisecond_to_time(data_json_list[i]["seriesData"][j][0])
+                    rate = data_json_list[i]["seriesData"][j][1]
                     try:
                         # 插入的SQL
-                        inserting_sql = "INSERT INTO chn_gov_bonds_rates_di(1m,p_day,submission_date)" \
+                        inserting_sql = "INSERT INTO chn_gov_bonds_rates_di (1m,p_day,submission_date)" \
                                         "VALUES ('%s','%s','%s')" % (
-                                        rate,p_day,today)
+                                        rate,p_day,self.today)
                         db_operator.DBOperator().operate("insert", "financial_data", inserting_sql)
 
                     except Exception as e:
                         # 日志记录
-                        msg = '收集国债到期收益率(1月期)失败' + '  ' + str(e)
+                        msg = '收集国债到期收益率(1月期)， 插入 '+str(p_day)+'的数据 失败' + '  ' + str(e)
                         custom_logger.CustomLogger().log_writter(msg, 'error')
 
             # 其它组到期信息，需要更新数据库
             else:
-                for j in data_json_list[i]["seriesData"]:
+                # 优先更新最新日期的数据
+                for j in range(len(data_json_list[i]["seriesData"]) - 1, -1+isOnlyToday, -1):
                     # 时间和利率信息如下
                     # [[1633622400000,1.776],[1633708800000,1.7659],,,]
-                    p_day = self.millisecond_to_time(j[0])
-                    rate = j[1]
+                    p_day = self.millisecond_to_time(data_json_list[i]["seriesData"][j][0])
+                    rate = data_json_list[i]["seriesData"][j][1]
                     try:
-                        # 插入的SQL
-                        # todo i=1, 插入2m; i=2, 插入3m; i=3, 插入6m;
-                        inserting_sql = "UPDATE chn_gov_bonds_rates_di SET(1m,p_day,submission_date)" \
-                                        "VALUES ('%s','%s','%s')" % (
-                                        rate,p_day,today)
-                        db_operator.DBOperator().operate("update", "financial_data", inserting_sql)
+                        # 更新的SQL
+                        updating_sql = "UPDATE chn_gov_bonds_rates_di SET "+ term_order_dict[i]+" = '%s' where p_day = '%s'" % (rate, p_day)
+                        db_operator.DBOperator().operate("update", "financial_data", updating_sql)
 
                     except Exception as e:
                         # 日志记录
-                        msg = '收集国债到期收益率(1月期)失败' + '  ' + str(e)
+                        msg = '更新国债到期收益率('+term_order_dict[i] +'期)， 插入 '+str(p_day)+'的数据 失败' + '  ' + str(e)
                         custom_logger.CustomLogger().log_writter(msg, 'error')
+
+    def main(self):
+        # 从中国债券信息网收集 中国债券到期收益率主流程
+        # 首先判断 中国国债到期收益率表 是否为空
+        # 如果为空，从 2010-01-01 开始收集数据
+        # 如果不为空，仅 数据库中的已收集的最新交易日至今的国债收益率数据
+
+        try:
+            # 查询sql
+            selecting_sql = "SELECT COUNT(*) as total_rows FROM chn_gov_bonds_rates_di"
+            # 查询
+            selecting_result = db_operator.DBOperator().select_one("financial_data", selecting_sql)
+
+        except Exception as e:
+            # 日志记录
+            msg = "无法判断中国国债到期收益率表chn_gov_bonds_rates_di是否为空 " + '  ' + str(e)
+            custom_logger.CustomLogger().log_writter(msg, 'error')
+            return None
+
+
+        # 如果表格为空，收集从 2010-01-01至今的数据
+        if selecting_result["total_rows"] == 0:
+            self.call_bonds_interface_to_collect_all_historical_data(startDay = "2010-01-01", endDay = self.today, isOnlyToday=0)
+        else:
+            # 获取中国国债到期收益率表chn_gov_bonds_rates_di已收集的最新交易日
+            try:
+                # 查询sql
+                selecting_max_date_sql = "SELECT max(p_day) max_day FROM chn_gov_bonds_rates_di"
+                # 查询
+                selecting_max_date = db_operator.DBOperator().select_one("financial_data", selecting_max_date_sql)
+
+            except Exception as e:
+                # 日志记录
+                msg = "无法从中国国债到期收益率表chn_gov_bonds_rates_di已收集的最新交易日信息 " + '  ' + str(e)
+                custom_logger.CustomLogger().log_writter(msg, 'error')
+                return None
+
+            self.call_bonds_interface_to_collect_all_historical_data(startDay = str(selecting_max_date["max_day"]), endDay = self.today, isOnlyToday=1)
 
 
 
 if __name__ == '__main__':
     time_start = time.time()
     go = CollectCHNGovBondsRates()
-    go.call_bonds_interface_to_collect_all_historical_data()
+    #go.call_bonds_interface_to_collect_all_historical_data("2021-11-04","2021-11-05",isOnlyToday=1)
+    go.main()
     time_end = time.time()
     print('Time Cost: ' + str(time_end - time_start))
