97d8d10ad4824fd2965f237be6e92121ac450843
AdhocSun <1029791755@qq.com>
2021-11-28 19:03:10 +0800

    股债收益率, 已进入投资区间，在标题处增强提示

--- notification/notification_plan_after_trading.py
+++ notification/notification_plan_after_trading.py
@@ -16,15 +16,19 @@ class NotificationPlanAfterTrading:
     def equity_bond_yield_strategy_estimation_notification(self):
         # 择时策略, 股债收益率, 邮件通知，微信通知
 
-        # 股债收益率
-        time_strategy_msg = time_strategy_equity_bond_yield.TimeStrategyEquityBondYield().main()
-
         # 获取当前时间
         today = time.strftime("%Y-%m-%d", time.localtime())
+        # 通知的标题
+        title = ' 股债收益率'
+
+        # 股债收益率
+        time_strategy_msg = time_strategy_equity_bond_yield.TimeStrategyEquityBondYield().main()
+        if ("投资" in time_strategy_msg):
+            title = ' 股债收益率, 已进入投资区间'
 
         # 邮件发送所有估值信息
         try:
-            email_notification.EmailNotification().send_customized_content(' 股债收益率', time_strategy_msg)
+            email_notification.EmailNotification().send_customized_content(title, time_strategy_msg)
             # 日志记录
             log_msg = '成功, 成功发送' + today + ' 股债收益率数据至邮件'
             custom_logger.CustomLogger().log_writter(log_msg, 'info')
@@ -35,7 +39,7 @@ class NotificationPlanAfterTrading:
 
         # 微信推送所有估值信息
         try:
-            wechat_notification.WechatNotification().push_to_all(' 股债收益率', time_strategy_msg)
+            wechat_notification.WechatNotification().push_to_all(title, time_strategy_msg)
             # 日志记录
             log_msg = '成功, 成功推送' + today + ' 股债收益率数据至微信'
             custom_logger.CustomLogger().log_writter(log_msg, 'info')
