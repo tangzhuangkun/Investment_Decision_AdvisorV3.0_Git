7601efefc7568978de8b6614549f668052b4215d
AdhocSun <1029791755@qq.com>
2021-12-04 13:10:31 +0800

    读取标的池中关于股票的信息

---
+++ target_pool/read_collect_target_stock.py
@@ -0,0 +1,41 @@
+
+import sys
+sys.path.append('..')
+import target_pool.target as target
+
+class ReadCollectTargetStock:
+    # 读取标的池中关于股票的信息
+
+    def __init__(self):
+        pass
+
+    def get_stocks_and_their_names(self):
+        # 获取标的池中跟踪关注股票及他们的中文名称
+        # 输入：无
+        # 输出：字典形式。如，{'000002': ('sz000002', '万科A'), '600048': ('sh600048', '保利发展')}
+
+        target_stocks = target.target_stocks
+        tracking_stocks_names_dict = {}
+        for tracking_info in target_stocks:
+            tracking_stocks_names_dict[tracking_info] = target_stocks.get(tracking_info).get('exchange_location')+tracking_info,target_stocks.get(tracking_info).get('stock_name')
+        return tracking_stocks_names_dict
+
+    def get_stocks_valuation_method_and_trigger(self):
+        # 获取标的池中跟踪关注股票及对应的估值方式和触发条件
+        # 输入：无
+        # 输出：字典形式。如，{'000002': {'pb': (0.95, 0.5), 'pe': (1, 2)}, '600048': {'pb': (0.95, 0.5)}}
+
+        target_stocks = target.target_stocks
+        tracking_stocks_valuation_method_and_trigger = {}
+        for tracking_info in target_stocks:
+            tracking_stocks_valuation_method_and_trigger[tracking_info] = target_stocks.get(tracking_info).get(
+                'valuation_method_and_trigger')
+        return tracking_stocks_valuation_method_and_trigger
+
+
+
+if __name__ == '__main__':
+    go = ReadCollectTargetStock()
+    # result = go.get_stocks_and_their_names()
+    result = go.get_stocks_valuation_method_and_trigger()
+    print(result)
