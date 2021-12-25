ad54c3bbfa09913e44f6e235b1d145ff90bfe0eb
AdhocSun <1029791755@qq.com>
2021-12-25 21:31:16 +0800

    一些通用的，用于收集指数某些属性的方法, 修正了传入的参数格式

--- data_collector/data_collector_common_index_collector.py
+++ data_collector/data_collector_common_index_collector.py
@@ -12,15 +12,10 @@ class DataCollectorCommonIndexCollector:
     def __init__(self):
         pass
 
-    def get_index_latest_increasement_decreasement_rate(self, index_code):
+    def get_index_latest_increasement_decreasement_rate(self, index_code_with_location):
         # 获取指数最新的涨跌率
-        # index_code: 指数代码, 必须如 399965.XSHE，代码后面带上市地
+        # index_code_with_location: 指数代码(含上市地), 必须如 sz399965，代码前面带上市地
         # return: 最新的涨跌幅, 如 0.39% 即返回为 0.39
-        location_index_code = ''
-        if index_code[-5:] == '.XSHE':
-            location_index_code = 'sz'+index_code[:6]
-        elif index_code[-5:] == '.XSHG':
-            location_index_code = 'sh' + index_code[:6]
 
         # 伪装，隐藏UA和IP
         ip_address, ua = disguise.Disguise().get_one_IP_UA()
@@ -31,7 +26,7 @@ class DataCollectorCommonIndexCollector:
         # 接口返回: 指数名称，当前点数，当前价格，涨跌率，成交量（手），成交额（万元）；
         # 接口返回如： var hq_str_s_sz399997="中证白酒,17305.78,66.408,0.39,1882235,1917906";
         # 只取 涨跌率
-        url = 'https://hq.sinajs.cn/list=s_'+location_index_code
+        url = 'https://hq.sinajs.cn/list=s_'+index_code_with_location
         content = requests.get(url, headers=header, proxies=proxy)
         content_split = content.text.split(',')
         #return float(content_split[3])
@@ -40,7 +35,7 @@ class DataCollectorCommonIndexCollector:
 if __name__ == '__main__':
     time_start = time.time()
     go = DataCollectorCommonIndexCollector()
-    result = go.get_index_latest_increasement_decreasement_rate('399997.XSHE')
+    result = go.get_index_latest_increasement_decreasement_rate('sz399965')
     print(result)
     time_end = time.time()
     print('time:')
