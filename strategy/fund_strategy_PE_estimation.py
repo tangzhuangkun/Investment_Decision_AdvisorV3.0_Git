8149a766a342f72fc4cb1a509186cbb324d22bef
AdhocSun <1029791755@qq.com>
2021-12-26 15:47:45 +0800

    修正，指数基金策略，市盈率估值法

--- strategy/fund_strategy_PE_estimation.py
+++ strategy/fund_strategy_PE_estimation.py
@@ -5,12 +5,12 @@ import threading
 import sys
 sys.path.append("..")
 import database.db_operator as db_operator
-import data_collector.get_stock_real_time_indicator_from_xueqiu as xueqiu
-import target_pool.read_collect_target_fund as read_collect_target_fund
 import log.custom_logger as custom_logger
 import data_miner.data_miner_common_index_operator as data_miner_common_index_operator
 import data_miner.data_miner_common_db_operator as data_miner_common_db_operator
 import data_collector.data_collector_common_index_collector as data_collector_common_index_collector
+import data_collector.get_stock_real_time_indicator_from_interfaces as get_stock_real_time_indicator_from_interfaces
+import data_miner.data_miner_common_target_index_operator as data_miner_common_target_index_operator
 
 
 class FundStrategyPEEstimation:
@@ -21,15 +21,14 @@ class FundStrategyPEEstimation:
     def __init__(self):
         pass
 
-    def get_stock_historical_pe(self, stock_code, stock_name, day):
+    def get_stock_historical_pe(self, stock_code, day):
         # 提取股票的历史市盈率信息， 包括市盈率TTM, 扣非市盈率TTM
         # param: stock_code, 股票代码，如 000596
-        # param: stock_name， 股票名称，如 古井贡酒
         # param: day, 日期， 如 2020-09-01
         # 返回：市盈率TTM, 扣非市盈率TTM
         # 例如 [{'pe_ttm': Decimal('61.6921425379745000'), 'pe_ttm_nonrecurring': Decimal('65.5556071829405200')}]
         selecting_sql = "select pe_ttm, pe_ttm_nonrecurring from stocks_main_estimation_indexes_historical_data " \
-                        "where stock_code = '%s' and stock_name = '%s' and date = '%s' " % (stock_code, stock_name,day)
+                        "where stock_code = '%s' and date = '%s' " % (stock_code,day)
         pe_info = db_operator.DBOperator().select_all("financial_data", selecting_sql)
         return pe_info
 
@@ -59,7 +58,7 @@ class FundStrategyPEEstimation:
 
     def get_a_historical_date_index_PE(self, index_code, day):
         # 从已算好的数据库中获取某个历史日期的市盈率
-        # param: index_code 指数代码，399997.XSHE
+        # param: index_code 指数代码，399997
         # param: day, 日期， 如 2020-09-01
         # 返回 指数市盈率TTM, 扣非市盈率TTM
         # 如 (Decimal('56.39008'), Decimal('53.32287'))
@@ -86,12 +85,23 @@ class FundStrategyPEEstimation:
         # stock_weight： 默认参数，在指数中，该成分股权重，默认为0
         # threadLock：线程锁
 
+        '''
         # 通过抓取数据雪球页面，获取单个股票的实时滚动市盈率
         stock_real_time_pe_ttm = xueqiu.GetStockRealTimeIndicatorFromXueqiu().get_single_stock_real_time_indicator(stock_id, 'pe_ttm')
         # 如果获取的股票实时滚动市盈率不是数字，如’亏损‘
         if not self.is_a_number(stock_real_time_pe_ttm):
             # 股票实时滚动市盈率为0
             stock_real_time_pe_ttm = '0'
+            # 忽略亏损的成分股票的权重
+            stock_weight = 0
+        '''
+        # 从腾讯接口获取实时市净率估值数据
+        stock_real_time_pe_ttm = get_stock_real_time_indicator_from_interfaces.GetStockRealTimeIndicatorFromInterfaces().get_single_stock_real_time_indicator(
+            stock_id, 'pe_ttm')
+        # 如果获取的股票实时滚动市盈率小于0，即’亏损‘
+        if (decimal.Decimal(stock_real_time_pe_ttm)<0):
+            # 股票实时滚动市盈率为0
+            stock_real_time_pe_ttm = "0"
             # 忽略亏损的成分股票的权重
             stock_weight = 0
 
@@ -101,13 +111,12 @@ class FundStrategyPEEstimation:
         self.index_real_time_positive_pe_ttm += stock_weight * decimal.Decimal(stock_real_time_pe_ttm)
         # 累加有效（即为正值的）市盈率成分股权重
         self.index_positive_pe_ttm_weight += stock_weight
-
         # 释放锁，开启下一个线程
         threadLock.release()
 
     def calculate_real_time_index_pe_multiple_threads(self,index_code):
         # 多线程计算指数的实时市盈率
-        # index_code, 指数代码（1、6位数字+交易所代码；2、6位数字；例如： 399997.XSHE 或者 399997）
+        # index_code, 指数代码,如 399997
         # 输出，指数的实时市盈率， 如 70.5937989
 
         # 统计指数实时的市盈率
@@ -125,24 +134,19 @@ class FundStrategyPEEstimation:
 
         # 遍历指数的成分股
         for i in range(len(stocks_and_their_weights)):
-            # 用于储存股票代码，例如 sh600726
-            stock_id = ''
+            # 拼接股票代码，例如 sh600726
 
             # 获取成分股上市地，
-            # XSHG 上海证券交易所
-            # XSHE 深圳证券交易所
-            stock_exchange = stocks_and_their_weights[i]["global_stock_code"][7:11]
-            # 获取成分股代码，6位纯数字
-            stock_code = stocks_and_their_weights[i]["global_stock_code"][:6]
-            # 获取成分股权重
+            # sh 代表上海
+            # sz 代表深圳
+            stock_exchange_location = stocks_and_their_weights[i]["stock_exchange_location"]
+            # 获取成分股代码，6位纯数字， 000568
+            stock_code = stocks_and_their_weights[i]["stock_code"]
+            # 获取成分股权重,
             stock_weight = stocks_and_their_weights[i]["weight"]
 
-            # 将股票代码进行转换，
-            # 由 6位数字+4位上市地（如 000596.XSHE） ---> 2位上市地+6位数字（如 sz000596）
-            if stock_exchange == 'XSHG':
-                stock_id = 'sh' + stock_code
-            elif stock_exchange == 'XSHE':
-                stock_id = 'sz' + stock_code
+            # 将股票代码进行转换，如 sz000568
+            stock_id = stock_exchange_location + stock_code
 
             # 启动线程
             running_thread = threading.Thread(target=self.get_and_calculate_single_stock_pe_ttm_weight_in_index,
@@ -173,7 +177,7 @@ class FundStrategyPEEstimation:
 
     def get_last_trading_day_PE(self, index_code):
         # 获取当前指数上一个交易日的  PETTM 和 扣非市盈率
-        # index_code: 指数代码, 必须如 399965.XSHE，代码后面带上市地
+        # index_code: 指数代码, 必须如 399965
         # return： 如果有值，则返回tuple，(PETTM, 扣非市盈率)
         #          如果无值，则返回tuple，(0, 0)
 
@@ -199,9 +203,10 @@ class FundStrategyPEEstimation:
             custom_logger.CustomLogger().log_writter(log_msg, 'error')
             return (0,0)
 
-    def cal_the_PE_percentile_in_history(self, index_code):
+    def cal_the_PE_percentile_in_history(self, index_code, index_code_with_location):
         # 获取当前指数的实时PETTM， 并计算当前实时市盈率TTM在历史上的百分位水平，预估扣非市盈率，历史百分位，同比上个交易日涨跌幅
-        # index_code: 指数代码, 必须如 399965.XSHE，代码后面带上市地
+        # index_code: 指数代码, 如 399965
+        # index_code_with_location: 指数代码（含上市地），sz399965
         # return: 当前指数的实时PETTM， 在历史上的百分位水平，预估的实时扣非市盈率，历史百分位，同比上个交易日涨跌幅
         #         如 [Decimal('58.7022'), 0.6686, Decimal('58.7844'), 0.3837, Decimal('0.0014')]
 
@@ -252,7 +257,7 @@ class FundStrategyPEEstimation:
         last_trading_day_pe_ttm, last_trading_day_pe_ttm_nonrecurring = self.get_last_trading_day_PE(index_code)
 
         # 获取指数最新的涨跌率
-        index_latest_increasement_decreasement_rate = data_collector_common_index_collector.DataCollectorCommonIndexCollector().get_index_latest_increasement_decreasement_rate(index_code)
+        index_latest_increasement_decreasement_rate = data_collector_common_index_collector.DataCollectorCommonIndexCollector().get_index_latest_increasement_decreasement_rate(index_code_with_location)
 
         # 根据PETTM的同比涨跌幅，预估实时的扣非市盈率
         index_real_time_predictive_pe_ttm_nonrecurring = round(last_trading_day_pe_ttm_nonrecurring * (1 + index_latest_increasement_decreasement_rate/100), 5)
@@ -284,19 +289,26 @@ class FundStrategyPEEstimation:
         # return: 返回讯息
 
         # 获取标的池中跟踪关注指数及他们的中文名称
+
+        '''
         # 字典形式。如，{'399396.XSHE': '国证食品', '000932.XSHG': '中证主要消费',,,,}
         #indexes_and_their_names = read_collect_target_fund.ReadCollectTargetFund().get_indexes_and_their_names()
-        indexes_and_their_names = read_collect_target_fund.ReadCollectTargetFund().index_valuated_by_method('pe')
+        #indexes_and_their_names = read_collect_target_fund.ReadCollectTargetFund().index_valuated_by_method('pe')
         #indexes_and_their_names = {'399396.XSHE': '国证食品'}
+        '''
+
+        # [{'index_code': '399965', 'index_name': '中证800地产', 'index_code_with_init': 'sz399965', 'index_code_with_market_code': '399965.XSHE'},，，]
+        indexes_and_their_names = data_miner_common_target_index_operator.DataMinerCommonTargetIndexOperator().index_valuated_by_method(
+            'pe')
 
         # 拼接需要发送的指数实时动态市盈率信息
         indexes_and_real_time_PE_msg = '指数实时动态市盈率和自2010年来历史百分位： \n\n'
-        for index_code in indexes_and_their_names:
+        for index_code_info in indexes_and_their_names:
             # 获取 当前指数的实时PETTM， 在历史上的百分位水平，预估的实时扣非市盈率，历史百分位，同比上个交易日涨跌幅
             # 如 [Decimal('58.7022'), 0.6686, Decimal('58.7844'), 0.3837, Decimal('0.0014')]
-            pe_result_list = self.cal_the_PE_percentile_in_history(index_code)
+            pe_result_list = self.cal_the_PE_percentile_in_history(index_code_info["index_code"],index_code_info["index_code_with_init"])
             # 生成 讯息
-            indexes_and_real_time_PE_msg += indexes_and_their_names[index_code] + ":  \n"+ "同比上一个交易日: "+str(pe_result_list[4])+"%;" +"\n"+"--------"+"\n"+"实时动态市盈率: "+\
+            indexes_and_real_time_PE_msg += index_code_info["index_name"] + ":  \n"+ "同比上一个交易日: "+str(pe_result_list[4])+"%;" +"\n"+"--------"+"\n"+"实时动态市盈率: "+\
                                             str(pe_result_list[0])+ "\n"+"历史百分位: "+str(decimal.Decimal(pe_result_list[1]*100).quantize(decimal.Decimal('0.00')))+"%;"+"\n"+\
                                             "--------"+"\n"+"预估扣非市盈率: "+str(pe_result_list[2])+ "\n"+"历史百分位: "+\
                                             str(decimal.Decimal(pe_result_list[3]*100).quantize(decimal.Decimal('0.00')))+"%;"+ "\n\n"
@@ -310,13 +322,15 @@ class FundStrategyPEEstimation:
 if __name__ == '__main__':
     time_start = time.time()
     go = FundStrategyPEEstimation()
+    result = go.generate_PE_strategy_msg()
+    print(result)
     #result = go.get_index_constitute_stocks("399997")
     #print(result)
-    #result = go.get_stock_historical_pe("000596", "古井贡酒", "2020-11-16")
+    #result = go.get_stock_historical_pe("000596",  "2021-12-24")
     #print(result)
     #pe_ttm, pe_ttm_nonrecurring = go.calculate_a_historical_date_index_PE("399997","2021-02-10")
     #print(pe_ttm, pe_ttm_nonrecurring)
-    #result = go.calculate_real_time_index_pe_multiple_threads("399997.XSHE")
+    #result = go.calculate_real_time_index_pe_multiple_threads("399997")
     #print(result)
     #msg = go.calculate_all_tracking_index_funds_real_time_PE_and_generate_msg()
     #print(msg)
@@ -325,13 +339,13 @@ if __name__ == '__main__':
     #print(index_all_historical_pe_info)
     #result = go.calculate_a_historical_date_index_PE("399997","2021-05-31")
     #print(result)
-    #result = go.get_last_trading_day_PE("399997.XSHE")
+    #result = go.get_last_trading_day_PE("399997")
     #print(result)
-    #result = go.cal_the_PE_percentile_in_history("399997.XSHE")
+    #result = go.cal_the_PE_percentile_in_history("399997", "sz399997")
     #print(result)
-    result = go.calculate_all_tracking_index_funds_real_time_PE_and_generate_msg()
-    print(result)
-    #result = go.get_a_historical_date_index_PE('399997.XSHE','2021-10-22')
+    #result = go.calculate_all_tracking_index_funds_real_time_PE_and_generate_msg()
+    #print(result)
+    #result = go.get_a_historical_date_index_PE('399997','2021-12-24')
     #print(result)
     time_end = time.time()
     print('time:')
